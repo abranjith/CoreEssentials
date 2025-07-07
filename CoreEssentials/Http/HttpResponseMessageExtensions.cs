1eb50c8a3e7e01c39cbe5e8752c7607095bec016
Ranjith Bojraj <abranjith@gmail.com>
2025-07-06 20:12:28 -0500

    Result support in http

---
+++ CoreEssentials/Http/HttpResponseMessageExtensions.cs
@@ -0,0 +1,45 @@
+﻿using CoreEssentials.Result;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CoreEssentials.Http
+{
+    public static class HttpResponseMessageExtensions
+    {
+
+        /// <summary>
+        /// Converts an <see cref="HttpResponseMessage"/> into a <see cref="Result{TSuccess, TError}"/> object by
+        /// deserializing the response content into either a success or error type.
+        /// </summary>
+        /// <remarks>This method uses <see cref="HttpContent.ReadFromJsonAsync{T}(JsonSerializerOptions?,
+        /// CancellationToken)"/> to deserialize the response content. Ensure that the response content matches the
+        /// expected JSON structure for <typeparamref name="TSuccess"/> or <typeparamref name="TError"/>.</remarks>
+        /// <typeparam name="TSuccess">The type to which the response content is deserialized if the HTTP response indicates success.</typeparam>
+        /// <typeparam name="TError">The type to which the response content is deserialized if the HTTP response indicates failure.</typeparam>
+        /// <param name="response">The <see cref="HttpResponseMessage"/> to process.</param>
+        /// <param name="options">Optional <see cref="JsonSerializerOptions"/> to customize the JSON deserialization process. Defaults to <see
+        /// langword="null"/>.</param>
+        /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the asynchronous operation to complete.
+        /// Defaults to <see cref="CancellationToken.None"/>.</param>
+        /// <returns>A <see cref="Result{TSuccess, TError}"/> object containing the deserialized content. If the HTTP response
+        /// indicates success, the result contains the deserialized <typeparamref name="TSuccess"/> value. If the HTTP
+        /// response indicates failure, the result contains the deserialized <typeparamref name="TError"/> value.</returns>
+        public async static Task<Result<TSuccess, TError>> AsResult<TSuccess, TError>(this HttpResponseMessage response,
+            JsonSerializerOptions? options = default, CancellationToken cancellationToken = default)
+        {
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadFromJsonAsync<TSuccess>(options, cancellationToken);
+                return content ?? default!;
+            }
+            else
+            {
+                var content = await response.Content.ReadFromJsonAsync<TError>(options, cancellationToken);
+                return content ?? default!;
+            }
+        }
+    }
+}
--- CoreEssentials/Result/Result.cs
+++ CoreEssentials/Result/Result.cs
@@ -30,12 +30,36 @@ namespace CoreEssentials.Result
             }
         }
 
+        /// <summary>
+        /// Creates a successful result containing the specified value.
+        /// </summary>
+        /// <param name="value">The value to be encapsulated in the successful result.</param>
+        /// <returns>A <see cref="Result{TResult, TError}"/> instance representing a successful operation with the provided
+        /// value.</returns>
         public static Result<TResult, TError> Ok(TResult value) => new Result<TResult, TError>(value);
+
+        /// <summary>
+        /// Creates a result representing an error.
+        /// </summary>
+        /// <param name="error">The error value to associate with the result. Cannot be null.</param>
+        /// <returns>A <see cref="Result{TResult, TError}"/> instance containing the specified error.</returns>
         public static Result<TResult, TError> Err(TError error) => new Result<TResult, TError>(error);
 
+        /// <summary>
+        /// Determines whether the current operation was successful.
+        /// </summary>
+        /// <returns><see langword="true"/> if the operation was successful; otherwise, <see langword="false"/>.</returns>
         public bool IsOk() => _isSuccess;
+
+        /// <summary>
+        /// Determines whether the current operation resulted in an error.
+        /// </summary>
+        /// <returns><see langword="true"/> if the operation was unsuccessful; otherwise, <see langword="false"/>.</returns>
         public bool IsErr() => !_isSuccess;
 
+        /// <summary>
+        /// Gets the value of the result if the operation was successful.
+        /// </summary>
         public TResult Value
         {
             get
@@ -46,6 +70,9 @@ namespace CoreEssentials.Result
             }
         }
 
+        /// <summary>
+        /// Gets the error associated with a failed result.
+        /// </summary>
         public TError Error
         {
             get
@@ -56,8 +83,22 @@ namespace CoreEssentials.Result
             }
         }
 
+        /// <summary>
+        /// Returns the contained value if the operation was successful; otherwise, returns the specified default value.
+        /// </summary>
+        /// <param name="defaultValue">The value to return if the operation was not successful.</param>
+        /// <returns>The contained value if the operation was successful; otherwise, the specified default value.</returns>
         public TResult UnwrapOr(TResult defaultValue) => _isSuccess ? _value : defaultValue;
 
+        /// <summary>
+        /// Retrieves the result value if the operation was successful, or throws an exception if it failed.
+        /// </summary>
+        /// <remarks>If the operation was unsuccessful, this method throws the exception associated with
+        /// the failure,  or a generic exception containing the error details if no specific exception is
+        /// available.</remarks>
+        /// <returns>The result value of type <typeparamref name="TResult"/> if the operation was successful.</returns>
+        /// <exception cref="Exception">Thrown if the operation was unsuccessful and no specific exception is available. The exception message
+        /// contains the error details.</exception>
         public TResult Unwrap()
         {
             if(_isSuccess)
@@ -134,6 +175,24 @@ namespace CoreEssentials.Result
             return targetBaseExceptionType.IsAssignableFrom(loadedExceptionType);
         }
 
+        /// <summary>
+        /// Implicitly converts a value of type <typeparamref name="TResult"/> to a <see cref="Result{TResult,
+        /// TError}"/> instance.
+        /// </summary>
+        /// <param name="value">The value to be wrapped in a <see cref="Result{TResult, TError}"/> as a successful result.</param>
+        public static implicit operator Result<TResult, TError>(TResult value) => Ok(value);
+
+        /// <summary>
+        /// Implicitly converts a value of type <typeparamref name="TError"/> to a <see cref="Result{TResult, TError}"/>
+        /// representing an error.
+        /// </summary>
+        /// <remarks>This operator provides a convenient way to create an error result without explicitly
+        /// calling the <c>Err</c> method.</remarks>
+        /// <param name="error">The error value to be encapsulated in the <see cref="Result{TResult, TError}"/>.</param>
+        public static implicit operator Result<TResult, TError>(TError error) => Err(error);
+
+        #region :: Private methods ::
+
         private string? ErrorAsString()
         {
             if (_isSuccess)
@@ -141,8 +200,7 @@ namespace CoreEssentials.Result
             return _error?.ToString();
         }
 
-        public static implicit operator Result<TResult, TError>(TResult value) => Ok(value);
+        #endregion
 
-        public static implicit operator Result<TResult, TError>(TError error) => Err(error);
     }
 }
